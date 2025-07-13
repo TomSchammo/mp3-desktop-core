e27873d176b1095264b5062796ac5062a731413c
Tom Schammo <tom@tomschammo.com>
2025-07-13 18:06:46 +0200

    refactor: Precompute count and improve const-correctness

--- src/img_processing.cpp
+++ src/img_processing.cpp
@@ -5,9 +5,8 @@
 void downscale_area_average(uint8_t *src, uint32_t src_width, uint32_t src_height, uint8_t *dst,
                             uint32_t dst_width, uint32_t dst_height, int32_t channels) {
 
-
-  auto x_scale = static_cast<float>(src_width) / dst_width;
-  auto y_scale = static_cast<float>(src_height) / dst_height;
+  const auto x_scale = static_cast<float>(src_width) / dst_width;
+  const auto y_scale = static_cast<float>(src_height) / dst_height;
 
   // downscaling only
   assert(x_scale > 1.0f);
@@ -16,25 +15,25 @@ void downscale_area_average(uint8_t *src, uint32_t src_width, uint32_t src_heigh
   for (uint32_t y = 0; y < dst_height; y++) {
     for (uint32_t x = 0; x < dst_width; x++) {
 
-      auto src_x_start = static_cast<uint32_t>(x * x_scale);
-      auto src_y_start = static_cast<uint32_t>(y * y_scale);
+      const auto src_x_start = static_cast<uint32_t>(x * x_scale);
+      const auto src_y_start = static_cast<uint32_t>(y * y_scale);
 
-      auto src_x_end = std::min(static_cast<uint32_t>((x + 1) * x_scale), src_width);
-      auto src_y_end = std::min(static_cast<uint32_t>((y + 1) * y_scale), src_height);
+      const auto src_x_end = std::min(static_cast<uint32_t>((x + 1) * x_scale), src_width);
+      const auto src_y_end = std::min(static_cast<uint32_t>((y + 1) * y_scale), src_height);
+
+      const auto pixel_count = (src_x_end - src_x_start) * (src_y_end - src_y_start);
 
       for (int32_t c = 0; c < channels; c++) {
 
         uint32_t sum = 0;
-        uint32_t count = 0;
 
         for (int32_t sy = src_y_start; sy < src_y_end; sy++) {
           for (int32_t sx = src_x_start; sx < src_x_end; sx++) {
             sum += src[(sy * src_width + sx) * channels + c];
-            count++;
           }
         }
 
-        dst[(y * dst_width + x) * channels + c] = static_cast<uint8_t>(sum / count);
+        dst[(y * dst_width + x) * channels + c] = static_cast<uint8_t>(sum / pixel_count);
       }
     }
   }
